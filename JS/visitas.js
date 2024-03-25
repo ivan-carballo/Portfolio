8b8b3fe4f998693e3573770b704f44754b7ecfad
ivan-carballo <149379998+ivan-carballo@users.noreply.github.com>
2024-03-25 10:36:35 +0100

    Update visitas.js

--- JS/visitas.js
+++ JS/visitas.js
@@ -1,15 +1,13 @@
-// Verificar si el contador ya existe en el almacenamiento local
+// Ver si el contador existe en el almacenamiento local
 if(localStorage.getItem('visitas')) {
-    // El contador ya existe, por lo que incrementamos en 1
+    // Si el contador existe le sumamos 1
     let visitas = parseInt(localStorage.getItem('visitas')) + 1;
     localStorage.setItem('visitas', visitas);
 } else {
-    // Si es la primera visita, inicializamos el contador en 1
+    // Si es la primera visita, comenzamos el contador en 1
     localStorage.setItem('visitas', 1);
 }
 
 // Obtener el contador de visitas
 let contadorVisitas = localStorage.getItem('visitas');
-
-// Mostrar el contador en la página
 document.getElementById('contador_visitas').innerHTML = `<div id="bienvenido">¡Bienvenida/o!</div>Has visitado esta pagina ${contadorVisitas} veces`;
